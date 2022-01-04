d712138c0e7e3d1c3d43ee19e4ebee11af0cb3b8
Zumo09 <63203939+Zumo09@users.noreply.github.com>
2022-01-04 11:25:38 +0100

    u

--- feedbackprize/utils.py
+++ feedbackprize/utils.py
@@ -1,10 +1,15 @@
-from typing import List
+from typing import Dict, List, Tuple
 import pandas as pd
 
-def read_data() -> pd.DataFrame:
+def read_data_csv() -> pd.DataFrame:
     return pd.read_csv('dataset/train.csv')
 
-def read_file(file_id: str, folder: str = 'train') -> List[str]:
+def read_file(file_id: str, folder: str = 'train') -> str:
     with open(f'dataset/{folder}/{file_id}.txt', 'r') as f:
-        lines = f.readlines()
-    return lines
+        text = f.read()
+    return text
+
+def read_dataset() -> Tuple[pd.Series, pd.DataFrame]:
+    data = read_data_csv()
+    full_text = pd.Series({id: read_file(id) for id in data['id'].unique()})
+    return full_text, data
