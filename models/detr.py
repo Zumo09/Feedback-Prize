6f0d49ffa6c36612e34f31259c3fe1d666ff0f8d
Mattia Bertè <33131887+TiaBerte@users.noreply.github.com>
2022-02-11 19:32:40 +0100

    Update detr.py

--- models/detr.py
+++ models/detr.py
@@ -13,14 +13,15 @@ class Transformer(nn.Module):
         self.encoder = self.model.encoder
         self.decoder = self.model.decoder
 
-    def forward(self, enc_input_ids, query_embed):
+    def forward(self, enc_input_ids, query_embed, glob_enc_attn, glob_dec_attn):
 
-        enc = self.encoder(enc_input_ids)
+        enc = self.encoder(enc_input_ids, global_attention_mask=glob_enc_attn)
         tgt = torch.zeros_like(query_embed)
         tgt = tgt.unsqueeze(-1).permute(2, 0, 1)
         tgt = tgt + query_embed
-        dec = self.decoder(
-            inputs_embeds=tgt, encoder_hidden_states=enc["last_hidden_state"]
+        dec = self.decoder(inputs_embeds=tgt, encoder_hidden_states=enc["last_hidden_state"],
+                           global_attention_mask=glob_dec_attn
+            
         )
 
         return dec
@@ -39,8 +40,8 @@ class DETR(nn.Module):
         # output positional encodings (object queries)
         self.query_pos = nn.parameter.Parameter(torch.rand(100, encoder_hd))
 
-    def forward(self, inputs_ids):
-        h = self.transformer(inputs_ids, self.query_embed.weight)["last_hidden_state"]
+    def forward(self, inputs_ids, glob_enc_attn, glob_dec_attn):
+        h = self.transformer(inputs_ids, self.query_embed.weight, glob_enc_attn, glob_dec_attn)["last_hidden_state"]
 
         # finally project transformer outputs to class labels and bounding boxes
         return {
