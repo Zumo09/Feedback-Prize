e2d257d8cd4612be703581cd16cceba3600de13c
Zumo09 <63203939+Zumo09@users.noreply.github.com>
2022-02-13 18:46:27 +0100

    device bug

--- engine.py
+++ engine.py
@@ -41,10 +41,10 @@ def train_one_epoch(
         for doc in samples:
             inputs = tokenizer([doc]).to(device)
 
-            glob_enc_attn = torch.zeros(inputs.size()[1])
+            glob_enc_attn = torch.zeros(inputs.size()[1]).to(device)
             glob_enc_attn[0] = 1
 
-            glob_dec_attn = torch.ones(model.num_queries)
+            glob_dec_attn = torch.ones(model.num_queries).to(device)
 
             outputs.append(model(inputs, glob_enc_attn, glob_dec_attn))
 
